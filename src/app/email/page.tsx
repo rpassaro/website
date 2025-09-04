b224e6e5f2d15a62061437d779fe73070708242c
rpassaro <passaroryan@gmail.com>
2025-09-04 13:09:19 -0500

    updated email page

--- src/app/email/page.tsx
+++ src/app/email/page.tsx
@@ -11,7 +11,7 @@ export default function EmailPage() {
                 >
                     passaroryan@gmail.com
                 </a>
-                ,
+                !
             </p>
         </section>
     );
